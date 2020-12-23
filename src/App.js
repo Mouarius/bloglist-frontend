da8defd4152890e915839697eef8d8336290d671
mouarius <mouarius@gmail.com>
2020-12-24 00:25:01 +0100

    login step1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import './App.css'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [notificationMessage, setNotificationMessage] = useState(null)
+  const [username, setUsername] = useState([])
+  const [password, setPassword] = useState([])
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    const credentials = { username, password }
+    try {
+      const user = await loginService.login(credentials)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setNotificationMessage({ type: 'error', content: 'Wrong credentials' })
+      setTimeout(() => setNotificationMessage(null), 5000)
+    }
+  }
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+  const loginForm = () => (
+    <div id="login-form">
+      <h2>login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          Username:
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          Password:
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
     </div>
   )
+  const blogsList = () => (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  )
+  const loginInfo = () => <p>{user.name} is logged in.</p>
+
+  if (user === null) {
+    return loginForm()
+  } else {
+    return (
+      <div>
+        {loginInfo()}
+        {blogsList()}
+        <Notification message={notificationMessage} />
+      </div>
+    )
+  }
 }
 
 export default App
