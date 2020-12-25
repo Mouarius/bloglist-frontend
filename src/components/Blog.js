04e8874ba0856dceb2d9217ff5d1272d78eeb7c7
mouarius <mouarius@gmail.com>
2020-12-25 01:02:00 +0100

    refactoring blogs + details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,28 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    <strong>{blog.title}</strong> - {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [detailed, setDetailed] = useState(false)
+
+  const showWhenVisible = { display: detailed ? '' : 'none' }
+  const buttonLabel = detailed ? 'hide' : 'view'
+
+  const toggleDetails = () => {
+    setDetailed(!detailed)
+  }
+  return (
+    <div className="blog">
+      <strong>{blog.title}</strong> - {blog.author}
+      <button onClick={toggleDetails}>{buttonLabel}</button>
+      <div className="blog-details" style={showWhenVisible}>
+        <ul>
+          <li>
+            <a href={blog.url}>{blog.url}</a>
+          </li>
+          <li>likes : {blog.likes}</li>
+          <li> {blog.user.name}</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
